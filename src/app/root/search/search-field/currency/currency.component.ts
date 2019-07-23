8884bcd476415446d58bf70ac907fb2232cd8b01
Alexander <alexandersazonof0@gmail.com>
2019-07-23 10:37:47 +0300

    Currency icon added

--- src/app/root/search/search-field/currency/currency.component.ts
+++ src/app/root/search/search-field/currency/currency.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../../../../service/currency.service';
 
 @Component({
   selector: 'app-currency',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyComponent implements OnInit {
 
-  constructor() { }
+  private currency;
+  private nowPosition;
+  private lastPosition;
+
+  constructor(private currencyService: CurrencyService) {
+    this.currency = this.currencyService.getCurrency();
+    this.nowPosition = 0;
+    this.lastPosition = this.currencyService.getLastPosition();
+  }
 
   ngOnInit() {
   }
 
+  changeCurrency() {
+    if (this.nowPosition === this.lastPosition) {
+      this.nowPosition = 0;
+    }
+    this.nowPosition++;
+    this.currency = this.currencyService.getCurrencyByPosition(this.nowPosition);
+  }
 }
