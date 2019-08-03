7a195cbaba29eb9521fdc678ab23624c803b2145
Alexandr Sazonov <Alexandr_Sazonov@epam.com>
2019-08-03 14:15:11 +0300

    Changed scrol load logic

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {ClothesItemService} from "../service/clothesItem.service";
+import {ClothesItem} from '../class/clothesItem';
 
 @Component({
   selector: 'app-main',
@@ -8,9 +9,31 @@ import {ClothesItemService} from "../service/clothesItem.service";
 })
 export class RootComponent implements OnInit {
 
+  clothesItems: ClothesItem[];
+
   constructor(private clothesItemService: ClothesItemService) { }
 
   ngOnInit() {
+    this.clothesItemService.current.subscribe(clothesItems => this.clothesItems = clothesItems);
+
+    // !!!!!!!!!!!!! WHEN SERVER WILL RUN , NEED CHECK WHICH DATA REQUIRED
+
+    this.clothesItemService.recommendations().subscribe(data => {
+      this.clothesItems = data;
+    });
+  }
+
+  @HostListener('scroll', ['$event'])
+  scrollHandler($event) {
+    if (($event.target.offsetHeight + $event.target.scrollTop ) >= $event.target.scrollHeight) {
+      console.log('Load new clothes');
+
+      const searchValue = localStorage.getItem('searchValue');
+      this.clothesItemService.search(searchValue).subscribe(data => {
+        this.clothesItems = this.clothesItems.concat(data);
+        this.clothesItemService.changeClotheItems(this.clothesItems);
+      });
+    }
   }
 
 }
