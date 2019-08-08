a4bbdf17f5b1f5ab6a5f3708c63692dfc3a60e95
Alexander <alexandersazonof0@gmail.com>
2019-08-08 22:30:58 +0300

    Removed resize of image

--- src/app/root/sandbox/look-item/look-item.component.ts
+++ src/app/root/sandbox/look-item/look-item.component.ts
@@ -1,7 +1,4 @@
-import {Component, ElementRef, HostListener, Input, OnInit, ViewChild} from '@angular/core';
-import {ResizeEvent} from 'angular-resizable-element';
-import {ResizeService} from "../../../service/resize.service";
-
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-look-item',
@@ -10,34 +7,10 @@ import {ResizeService} from "../../../service/resize.service";
 })
 export class LookItemComponent implements OnInit {
 
-  public styleOfLook: object = {};
   @Input() image: string;
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  validate(event: ResizeEvent): boolean {
-    return !(!ResizeService.isProportionally(event) || !ResizeService.isValidate(event));
-  }
-
-
-  onResizeEnd(event: ResizeEvent): void {
-
-    this.styleOfLook = {
-      width: `${event.rectangle.width}px`,
-      height: `${event.rectangle.height}px`
-    };
-
-    this.focusWithTime();
-  }
-
-  focusWithTime() {
-    setTimeout(function () {
-      document.getElementById('box').focus();
-    }, 100);
-  }
-
-  eventOn
 }
